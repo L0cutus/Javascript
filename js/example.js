8c08cea3aa2438a1ff8345f3a3c49ee3913be64d
Stefano <mie.iscrizioni@gmail.com>
2016-02-08 13:29:47 +0100

    Files iniziali

---
+++ js/example.js
@@ -0,0 +1,17 @@
+var i = 10;
+while (i) {
+//  Document.write(i);
+  i--;
+}
+
+/**
+ * [[Description]]
+ * @param   {[[Type]]} var i [[Description]]
+ * @returns {[[Type]]} [[Description]]
+ */
+function inci(i){
+  return i++;
+}
+
+
+
